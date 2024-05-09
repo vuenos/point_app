830745bf6d4a16ca768a0a476b1bbec725975bcb
Jintae.kim <vuenos@naver.com>
2024-05-09 17:06:14 +0900

    서버요청방식 async로 변경, 응답상태별 메시지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import styles from "./page.module.css";
 import axios from "axios";
 
 export default function Home() {
-  const [msg, setMsg] = useState(null);
-  const [callStatus, setCallStatus] = useState<boolean>(false);
+  const [msg, setMsg] = useState<string>("");
+  const [callStatus, setCallStatus] = useState<string>();
   const [array1, setArray1] = useState([0,1,2,3,4,5,6]);
 
   const CallStackSuccess = () => (
@@ -16,16 +16,20 @@ export default function Home() {
     <div style={{ color: "red" }}>🧭 {msg}</div>
   )
   
-  const callDataHandler = () => {
-    axios.get("http://localhost:5500/getdata").then((res) => {
-      console.log(res.data);
-      setMsg(res.data);
-      setCallStatus(true);
-    }).catch((error) => {
-      console.log(error.message);
-      setMsg(error.message);
-      setCallStatus(false);
-    });
+  const callDataHandler = async () => {
+    try {
+      const { data, status } = await axios.get("http://localhost:5500/getdata");
+
+      if (status === 200) {
+        console.log(data);
+        setMsg(data);
+        setCallStatus("suceess");
+      }
+    } catch(error: any) {
+      console.log(error);
+      setMsg("서버 통신을 실패하였습니다.");
+      setCallStatus("failed");
+    };
   };
 
   useEffect(() => {
@@ -39,7 +43,10 @@ export default function Home() {
       <button onClick={callDataHandler}>데이터 요청</button>
 
       <div>
-        {callStatus ? <CallStackSuccess /> : <CallStackFailed />}
+        {msg && (callStatus === "suceess") ? <CallStackSuccess /> 
+          : (callStatus === "failed") ? <CallStackFailed />
+          : null
+        }
       </div>
     </main>
   );
