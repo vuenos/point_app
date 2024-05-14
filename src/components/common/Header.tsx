387eed63f06f7401835dc8c72d86a4390125a3e8
Jintae.kim <vuenos@naver.com>
2024-05-14 10:54:42 +0900

    install @emotion/react

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
-        <>
+        <div>
             <h1>Header</h1>
-        </>
+        </div>
     )
 }
---
+++ src/style/common/ResetStyle.ts
@@ -0,0 +1,9 @@
+import { css } from "@emotion/css";
+
+const resetStyle = css({
+    "*": {
+        boxSizing: "border-box",
+    }
+})
+
+export default resetStyle;
