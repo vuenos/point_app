b82ee515af1cefd2ed390e11a326d130ae23db24
Jintae.kim <vuenos@naver.com>
2024-05-14 17:03:12 +0900

    global styles define

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -6,6 +6,61 @@ const GlobalStyles = createGlobalStyle`
     ${ResetStyles}
     * {
         box-sizing: border-box;
+        -webkit-tap-highlight-color: transparent;
+    }
+
+    body {
+        font-family: Pretendard;
+    }
+
+    strong {
+        font-weight: 700;
+    }
+
+    a,a:link,a:visited,a:hover,a:focus,a:active {
+        -webkit-text-decoration: none;
+        text-decoration: none;
+    }
+
+    input,button,select,textarea {
+        display: inline-block;
+        margin: 0;
+        padding: 0;
+        background-color: transparent;
+        border: 0;
+        border-radius: 0;
+        font-family: inherit;
+        font-size: inherit;
+        color: inherit;
+        outline: none;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        -ms-appearance: none;
+        appearance: none;
+        -webkit-text-size-adjust: none;
+        -moz-text-size-adjust: none;
+        -ms-text-size-adjust: none;
+        text-size-adjust: none;
+    }
+
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        -ms-appearance: none;
+        appearance: none;
+    }
+
+    button {
+        text-align: center;
+    }
+
+    button:not(:disabled) {
+        cursor: pointer;
+    }
+
+    :root {
+        -webkit-print-color-scheme: light only;
+        color-scheme: light only;
     }
 `;
 
