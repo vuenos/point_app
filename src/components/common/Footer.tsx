17dfc54e6886b0016d877dc2cd5fb88c8036b5b4
Jintae.kim <vuenos@naver.com>
2024-05-16 17:17:50 +0900

    footer style define

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -5,12 +5,14 @@ import { FooterSection } from "@/styles/FooterStyles";
 export default function Footer() {
     return (
         <FooterSection>
-            <div>
-                <span className="small-bi">App</span>
-                <div className="copy">
-                    &copy;Pointapp
-                </div>
+            <div className="small-bi">
+                <Link href="/">App</Link>
             </div>
+            
+            <div className="copy">
+                &copy; Pointapp
+            </div>
+
             <nav>
                 <Link href="/agreement">Agreement</Link>
                 <Link href="/privacy">privacy</Link>
--- src/styles/FooterStyles.ts
+++ src/styles/FooterStyles.ts
@@ -3,10 +3,45 @@ import { styled } from "styled-components";
 const FooterSection = styled.div`
     display: flex;
     justify-content: space-between;
-    align-items: flex-start;
+    align-items: center;
     height: 96px;
     padding: 24px;
     background-color: #f9fafb;
+
+    .small-bi {
+        flex: 0 1 40%;
+
+        a {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 40px;
+            height: 40px;
+            background-color: #000;
+            text-align: center;
+            color: #ffffff;
+            font-size: 12px;
+            border-radius: 100%;
+            color: #ffffff;
+        }
+    }
+
+    .copy {
+        color: #999;
+    }
+
+    nav {
+        flex: 0 1 40%;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+
+        a {
+            margin-left: 24px;
+            color: #333;
+            font-weight: 700;
+        }
+    }
 `;
 
 export {
