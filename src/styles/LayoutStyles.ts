9c53fdd8063afcf9a6020907f52da9777eca5742
jintaekim <vuenos@naver.com>
2024-05-17 17:15:12 +0900

    Modified Contents Heading

--- src/styles/LayoutStyles.ts
+++ src/styles/LayoutStyles.ts
@@ -8,6 +8,14 @@ const MainSection = styled.main`
     height: calc(100vh - 96px);
     padding: 100px 24px 48px 24px;
     background-color: #ffffff;
+    
+    h2 {
+        width: 100%;
+        margin-top: 24px;
+        margin-bottom: 32px;
+        text-align: center;
+        letter-spacing: 0.09em;
+    }
 `;
 
 export {
