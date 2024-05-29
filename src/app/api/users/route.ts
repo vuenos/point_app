2ef054f5c1986220d78857c3e317b8498ac90fb5
jintae kim <vuenos@naver.com>
2024-05-29 23:38:21 +0900

    change keyword Request, Response to NextRequest, NextResponse

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,10 +1,10 @@
 import {NextRequest, NextResponse} from "next/server";
 import connect from "@/utils/connectMongDB"
 
-export async function GET(request: Request) {
+export async function GET(req: NextRequest) {
     const client = await connect;
     const cursor = await client.db("test").collection("users").find();
     const getUsers = await cursor.toArray();
 
-    return Response.json(getUsers);
+    return NextResponse.json(getUsers);
 }
