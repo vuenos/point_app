d5b331ce4f898b10d1cbdb5444b009397a28f562
Jintae KIm <vuenos@naver.com>
2024-05-30 09:43:51 +0900

    add loader in Users page

--- src/app/member/users/page.tsx
+++ src/app/member/users/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Loader from "@/components/common/Loader";
 
 type Users = {
     userEmail: string,
@@ -11,12 +12,15 @@ type Users = {
 
 export default function GetUsers () {
     const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const getUsersHandler = async () => {
+        setLoading(true);
         try {
             const { data, status } = await axios.get("/api/users/");
             // const data: Users[] = await respnose.json();
             if (status === 200) {
+                setLoading(false);
                 setUsers(data);
                 console.log(`USER_DATA::: ${data}`);
             }
@@ -33,13 +37,23 @@ export default function GetUsers () {
 
     return (
         <>
-            <ul>
-                {users && users.map((user) => (
-                    <li key={user._id.toString()}>
-                        {user.name}({user.email})
-                    </li>
-                ))}
-            </ul>
+            {loading ? (
+                <Loader />
+            ) : (
+                <>
+                    {users ?
+                        <ul>
+                            {users && users.map((user) => (
+                                <li key={user._id.toString()}>
+                                    {user.name}({user.email})
+                                </li>
+                            ))}
+                        </ul>
+                        : <div>No data</div>
+                    }
+                </>
+            )}
+            
         </>
     )
 }
---
+++ src/components/common/Loader.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { RiLoader4Line } from "react-icons/ri";
+
+export default function Loader() {
+    return (
+        <div><RiLoader4Line /></div>
+    )
+}
