83c0e379d1571f0a942edc59bca8e33f522fb6a3
Jintae KIm <vuenos@naver.com>
2024-06-05 16:20:45 +0900

    reset login page style

--- src/app/member/login/page.tsx
+++ src/app/member/login/page.tsx
@@ -15,8 +15,6 @@ const Signin = () => {
   const router = useRouter();
   const { data: session } = useSession();
 
-  const labelStyles = "w-full text-sm";
-
   useEffect(() => {
     if (session?.user) {
       router.push("/");
@@ -42,34 +40,27 @@ const Signin = () => {
   };
 
   return (
-    <section className="w-full h-screen flex items-center justify-center">
-      <form
-        className="p-6 xs:p-10 w-full max-w-[350px] flex flex-col justify-between items-center gap-2.5 
-        border border-solid border-[#242424] bg-[#0a0a0a] rounded"
-        onSubmit={handleSubmit}
-      >
-        {error && <div className="">{error}</div>}
-        <h1 className="mb-5 w-full text-2xl font-bold">Signin</h1>
+    <section>
+      <form onSubmit={handleSubmit}>
+        {error && <div>{error}</div>}
+        <h1>Signin</h1>
 
-        <label className={labelStyles}>Email:</label>
+        <label>Email:</label>
         <input
           type="email"
           placeholder="Email"
-          className="w-full h-8 border border-solid border-[#242424] py-1 px-2.5 rounded bg-black text-[13px]"
           name="email"
         />
 
-        <label className={labelStyles}>Password:</label>
-        <div className="flex w-full">
+        <label>Password:</label>
+        <div>
           <input
             type={showPassword ? "text" : "password"}
             placeholder="Password"
-            className="w-full h-8 border border-solid border-[#242424] py-1 px-2.5 rounded-l bg-black text-[13px]"
             name="password"
           />
           <button
-            className="w-2/12 border-y border-r border-solid border-[#242424] bg-black rounded-r 
-          flex items-center justify-center transition duration-150 ease hover:bg-[#1A1A1A]"
+            type="button"
             onClick={(e) => {
               e.preventDefault();
               setShowPassword(!showPassword)
@@ -78,27 +69,20 @@ const Signin = () => {
             {showPassword ? <BiSolidHide /> : <BiSolidShow />}
           </button>
         </div>
-        <button className="w-full bg-black border border-solid border-[#242424] py-1.5 mt-2.5 rounded
-        transition duration-150 ease hover:bg-[#1A1A1A] text-[13px]"
-        >
+        <button type="submit">
           Signup
         </button>
-
-        <div className="w-full h-10 relative flex items-center justify-center">
-          <div className="absolute h-px w-full top-2/4 bg-[#242424]"></div>
-          <p className="w-8 h-6 bg-[#0a0a0a] z-10 flex items-center justify-center">or</p>
-        </div>
+        
 
         <button
-          className="flex py-2 px-4 text-sm align-middle items-center rounded text-999 bg-black 
-          border border-solid border-[#242424] transition duration-150 ease hover:bg-[#1A1A1A] gap-3"
+          type="button"
           onClick={(e) => {
             e.preventDefault();
             signIn("google")
           }}>
-          <BiLogoGoogle className="text-2xl" /> Sign in with Google
+          <BiLogoGoogle /> Sign in with Google
         </button>
-        <Link href="/register" className="text-sm text-[#888] transition duration-150 ease hover:text-white">
+        <Link href="/member/join">
           Don&apos;t have an account?
         </Link>
       </form>
