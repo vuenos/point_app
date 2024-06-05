bdd0292875cbfc9b17191c7cce14aac42d045fa8
Jintae KIm <vuenos@naver.com>
2024-06-05 17:37:29 +0900

    add session options

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -41,6 +41,8 @@ export const authOptions: NextAuthOptions = {
     },
     session: {
         strategy: "jwt",
+        maxAge: 30 * 24 * 60 * 60, // 30 days
+        updateAge: 24 * 60 * 60, // 24 hours
     },
     callbacks: {
         async jwt({ token, user, session, trigger }) {
