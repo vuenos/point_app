7dba545405ddbe5eacad6d299ce80c3eb53db58a
jintae kim <vuenos@naver.com>
2024-07-08 21:03:21 +0900

    apply eslint

--- src/app/api/mypage/route.ts
+++ src/app/api/mypage/route.ts
@@ -1,30 +1,30 @@
-import { connectDB } from "@/utils/connectMongDB";
-import { NextRequest, NextResponse } from "next/server";
+import {connectDB} from "@/utils/connectMongDB";
+import {NextRequest, NextResponse} from "next/server";
 import Card from "@/models/cardModel";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/libs/auth";
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/libs/auth";
 
 export async function GET(req, res) {
-    // return NextResponse.json([
-    //     {cardNumber: "983264923874932"},
-    //     {cardNumber: "222222"}
-    // ])
+  // return NextResponse.json([
+  //     {cardNumber: "983264923874932"},
+  //     {cardNumber: "222222"}
+  // ])
 
-    const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-    try {
-        await connectDB();
+  try {
+    await connectDB();
 
-        if(session) {
-            const cards = await Card.find({ userId: session?.user._id });
-            return NextResponse.json(
-                cards,
-                { status: 200 }
-            );
-        } else {
-            res.status(401);
-        }
-    } catch (error) {
-        return NextResponse.json({error: "Server Error"}, { status: 500});
+    if (session) {
+      const cards = await Card.find({userId: session?.user._id});
+      return NextResponse.json(
+        cards,
+        {status: 200}
+      );
+    } else {
+      res.status(401);
     }
+  } catch (error) {
+    return NextResponse.json({error: "Server Error"}, {status: 500});
+  }
 };
--- src/app/member/mypage/page.tsx
+++ src/app/member/mypage/page.tsx
@@ -1,67 +1,67 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { useSession } from "next-auth/react";
-import axios, { AxiosError } from "axios";
-import { useRouter } from "next/navigation";
-import { CalloutBox } from "@/styles/ComponentStyles";
+import {useEffect, useState} from "react";
+import {useSession} from "next-auth/react";
+import axios, {AxiosError} from "axios";
+import {useRouter} from "next/navigation";
+import {CalloutBox} from "@/styles/ComponentStyles";
 
 
 export default function Mypage() {
-    const [cards, setCards] = useState([]);
-    const [error, setError] = useState();
-    const { data: session, status } = useSession();
-    const router = useRouter();
+  const [cards, setCards] = useState([]);
+  const [error, setError] = useState();
+  const {data: session, status} = useSession();
+  const router = useRouter();
 
-    const getUserCards = async () => {
+  const getUserCards = async () => {
 
-        try {
-            const res = await axios.get(`${process.env.NEXT_PUBLIC_APP_URL}/api/mypage`);
-            if (res.status === 200) {
-                setCards(res.data);
-                console.log("DATA::", res.data)
-            }
-        } catch (error) {
-            console.log(error.message);
-            if (error instanceof AxiosError) {
-                const errorMessage = error.response?.data.message;
-                setError(errorMessage);
-            }
-        }
-    };
+    try {
+      const res = await axios.get(`${process.env.NEXT_PUBLIC_APP_URL}/api/mypage`);
+      if (res.status === 200) {
+        setCards(res.data);
+        console.log("DATA::", res.data)
+      }
+    } catch (error) {
+      console.log(error.message);
+      if (error instanceof AxiosError) {
+        const errorMessage = error.response?.data.message;
+        setError(errorMessage);
+      }
+    }
+  };
 
-    useEffect(() => {        
-        if (status === "unauthenticated") {
-            router.push("/member/login");
-        }
-    }, [status]);
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/member/login");
+    }
+  }, [status]);
 
-    useEffect(() => {
-        if (session) {
-            getUserCards();
-        }
-    }, [session]);
+  useEffect(() => {
+    if (session) {
+      getUserCards();
+    }
+  }, [session]);
 
-    return (
+  return (
+    <>
+      <h2>My page <sup>{session?.user._id}</sup></h2>
+      {cards.length === 0 ? (
         <>
-            <h2>My page <sup>{session?.user._id}</sup></h2>
-            {cards.length === 0 ? (
-                <>
-                    {error && <CalloutBox className="error"><h4 className="title">Error</h4> {error}</CalloutBox>}
-                    <div>No cards found</div>
-                </>
-            ) : (
-                <ul>
-                    {cards.map((card) => (
-                        <li key={card._id}>
-                            <div>{card.cardNumber}</div>
-                            <div>{card.cvc}</div>
-                            <div>{card.createdAt.slice(0, 10, "")}</div>
-                            <div>{card.userId}</div>
-                        </li>
-                    ))}
-                </ul>
-            )}
+          {error && <CalloutBox className="error"><h4 className="title">Error</h4> {error}</CalloutBox>}
+          <div>No cards found</div>
         </>
-    );
+      ) : (
+        <ul>
+          {cards.map((card) => (
+            <li key={card._id}>
+              <div>{card.cardNumber}</div>
+              <div>{card.cvc}</div>
+              <div>{card.createdAt.slice(0, 10, "")}</div>
+              <div>{card.userId}</div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
